4e832c69e6d63c62b0a0d7eedef47868fce941c3
Enoch18 <sokoenock@gmail.com>
2021-10-23 14:15:25 +0200

    Added the user profile on the sidebar

--- views/pages/admin/layouts/sidebar.php
+++ views/pages/admin/layouts/sidebar.php
@@ -38,8 +38,10 @@
 
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
-                    <span class="ml-2">Integrations</span>
+                <svg class="svg-icon" viewBox="0 0 20 20" width="24" height="24">
+					<path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path>
+				</svg>
+                <span class="ml-2">User Profile</span>
                 </a>
             </li>
         </ul>
